07d9f1d32aedb65c1e856bb87417b211dfb489e3
Your Name <you@example.com>
2024-11-18 16:27:40 -0300

    A primeira fase da aula VUE.JS

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+
+export default {
+  components: {
+    Banner,
+    ConteudoPrincipal
+  }
+};
+</script>
+
+<template>
+  <div>
+    <Banner />
+    <ConteudoPrincipal />
+  </div>
+</template>
+
+<style scoped>
+/* Estilos do componente principal */
+</style>
